d973a7a5e73ff1c35205f648b59860e1a056cfc4
Dorsa Maleki <a.malekidorsa@gmail.com>
2018-08-21 14:25:17 +0430

    second

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 let suits = ["Diamond", "Hearts", "Spades", "Clubs"];
 
-let values = ["Ace", "Two", "three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Jack"];
+let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Jack"];
 
 
 let textArea = document.getElementById('text-area');
@@ -15,10 +15,48 @@ let hitButton = document.getElementById('hit-button');
 let stayButton = document.getElementById('stay-button');
 
 
-hitButton.style.display = 'none';
-stayButton.style.display = 'none';
+let gameStarted = false;
+
+showStatus();
+
+newGameButton.addEventListener('click',function(){
+
+  gameStarted = true;
+  showStatus();
+});
 
 
+
+
+
+
+
+function showStatus(){
+
+
+  if (!gameStarted){
+
+    hitButton.style.display = 'none';
+    stayButton.style.display = 'none';
+
+  }
+
+  else if (gameStarted){
+
+    hitButton.style.display = 'inline';
+    stayButton.style.display = 'inline';
+    newGameButton.style.display = 'none';
+
+
+    textArea.innerText = "Dealer Cards : " + DealerCardsString + "\n" + "Dealer score:" + getDealerScore() + "\n" + "\n" +
+                        "player Cards : " + playerCardsString + "\n" + "player score :" + getPlayerScore();
+
+
+  }
+
+}
+
+//show status badan 
 
 
 function createDeck(){
@@ -52,7 +90,7 @@ let Deck = createDeck();
 
 function getCardString(card){
 
-  return card.value + "of" + card.suit;
+  return card.value + " of " + card.suit;
 
 }
 
@@ -108,23 +146,101 @@ let playerCardsString = ''; //ye stringe
 
 for (let i = 0; i < playerCards.length; i++){
   
-  playerCardsString = playerCardsString + " , " +getCardString(playerCards[i]); 
+  playerCardsString = playerCardsString + " \n " + getCardString(playerCards[i]); 
 
 }
 
 
-console.log(playerCardsString);
+//console.log(playerCardsString);
 
 
 let DealerCardsString = '';
 
 for (let i = 0; i < DealerCards.length; i++){
   
-  DealerCardsString = DealerCardsString + " , " +getCardString(DealerCards[i]);
+  DealerCardsString = DealerCardsString + " \n " + getCardString(DealerCards[i]);
 
 }
 
 
-console.log(DealerCardsString);
+
+//console.log(DealerCardsString);
+
+
+
+function getCardNumericValue(card){
+
+  switch(card.value){
+
+    case 'Ace':
+      return 1;
+
+    case 'Two':
+      return 2;
+
+    case 'Three':
+      return 3;
+
+    case 'Four':
+      return 4;
+
+    case 'Five':
+      return 5;
+
+    case 'Six':
+      return 6;
+
+    case 'Seven':
+      return 7
+
+    case 'Eight':
+      return 8;
+
+    case 'Nine':
+      return 9;
+
+    default:
+      return 10;
+
+  }
+
+}
+
+
+
+/* bara teste tabe bala 
+
+let test = getCardNumericValue(playerCards[0]);
+console.log(test);*/
+
+
+/*function getScore(){
+
+  let playerScore = getCardNumericValue(playerCards[0]) + getCardNumericValue(playerCards[1]) ;
+  let DealerScore = getCardNumericValue(DealerCards[0]) + getCardNumericValue(DealerCards[1]) ;
+//  console.log(playerScore);
+
+}*/
+
+//console.log(getScore());
+
+
+function getPlayerScore(){
+
+  let playerScore = getCardNumericValue(playerCards[0]) + getCardNumericValue(playerCards[1]) ;
+  return playerScore;
+}
+
+function getDealerScore(){
+
+  let DealerScore = getCardNumericValue(DealerCards[0]) + getCardNumericValue(DealerCards[1]) ;
+  return DealerScore;
+}
+
+
+
+
+
+
 
 
